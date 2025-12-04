978ebd7017574efccf04c033f2fb850bf224199e
Ted Pokorski <tpokorski1@gmail.com>
2025-12-04 13:16:32 -0500

    furst commit

--- obj/Debug/net10.0/MyFirstApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc2f9e72d5613dfd604958a06ef818e04481275")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
