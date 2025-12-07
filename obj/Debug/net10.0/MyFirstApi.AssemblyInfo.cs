7dd58c6cf8d2ec19b22b605498b326918e048072
Ted Pokorski <tpokorski1@gmail.com>
2025-12-07 15:25:42 -0500

    added _ViewImports file

--- obj/Debug/net10.0/MyFirstApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11db5511f738ee8b14256be556d9d98f9f8e312f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5f870f5ee400600efcbd646f594386fe1f79b52")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
